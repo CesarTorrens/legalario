8a70ebc176e1288e3c9bb14ac3fb6c78cd0482ea
Cesar Torrens <cesar.a.torrens@gmail.com>
2023-12-25 11:03:25 -0500

    try 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     }
   };
 
-  const handleClickOpen = (key?: string) => {
+  const handleClickOpen = (key?: any) => {
     if (key === "alert") {
       setOpenAlert(true);
     } else {
